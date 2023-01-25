fe0f1373e3ca0f11ea5ad28dbf5f6173a1735992
Alirio Lopez <Alopez922@hotmail.com>
2023-01-25 11:40:49 -0500

    formulario

---
+++ main.js
@@ -0,0 +1,110 @@
+Vue.component("usuario-add",{
+    template:`
+    <div>
+    
+    <form>
+    <table>
+    <th>
+    <label>Nombre</label>
+    </th>
+    <th>
+    <label>Apellido</label>
+    </th>
+    <th>
+    <label>Usuario</label>
+    </th>
+    <th>
+    <label>Fecha de nacimiento</label>
+    </th>
+    <tr>
+    <td>
+    <input type="text" v-model="nombre">
+    </td>
+    <td>
+    <input type="text" v-model="apellido">
+    </td>
+    <td>
+    <input type="text" v-model="usuario">
+    </td>
+    <td>
+    <input type="date" v-model="edad">
+    </td>
+    </tr>
+
+
+    </form>
+    <button @click="onClick">Guardar</button>    
+    </div>
+    `,
+    data:function(){
+        return{
+            nombre:null,
+            apellido:null,
+            usuario:null,
+            edad:null
+        }
+    },
+
+    methods:{
+        onClick:function(){
+            this.$emit("new",{nombre:this.nombre, apellido:this.apellido, usuario:this.usuario, edad:Math.abs(this.edad.slice(0,4)-2023)})
+
+        }
+    }
+
+})
+
+
+Vue.component("usuario-item",{
+    props:["usuario"],
+    template:`
+    <li>
+        <div>
+        Nombre Completo: {{usuario.nombre}} {{usuario.apellido}} 
+        Usuario:{{usuario.usuario}}
+        Edad:{{usuario.edad}}  
+        </div>
+    </li>
+    `
+})
+
+Vue.component("usuario-list",{
+    props:["usuarios"],
+    template:`
+    
+    <ul>
+    <usuario-item 
+    v-for="(usuario,index) in usuarios"
+    :key="index"
+    :usuario="usuario">
+    </usuario-item>
+    </ul>
+    `
+})
+
+const app = new Vue({
+    el:"#app",
+    data:{
+        usuarios:[]
+    },
+
+    template:`
+    <div>
+    <usuario-list :usuarios="usuarios"></usuario-list>
+    <usuario-add @new="addNewUsuario"></usuario-add>
+    </div>
+    `,
+
+    methods:{
+        addNewUsuario:function(usuario){
+            let usuariorepe = this.usuarios.filter(el=>el.usuario === usuario.usuario)
+            if(usuariorepe.length > 0){
+                return alert(`usuario existente`)
+            }else{
+                this.usuarios.push(usuario)
+            }
+           
+        }
+    }
+
+})
